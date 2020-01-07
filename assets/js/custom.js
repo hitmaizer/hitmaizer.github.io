a4752cd30ce313fdbe361d9db809396a5db567ac
Jose Alves <jose.sousalves@gmail.com>
2020-01-07 16:01:37 +0000

    test

---
+++ assets/js/custom.js
@@ -0,0 +1,2 @@
+"use strict";$(document).ready(function(){$(document).on("click","a[href^=\"#\"]",function(a){a.preventDefault(),$("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top+-54},1e3)})});
+//# sourceMappingURL=custom.js.map
